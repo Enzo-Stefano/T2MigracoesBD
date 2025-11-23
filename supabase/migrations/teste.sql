f4549ad6f2226aa7e1dfd6dfa1beabf132a9852a
mts17c <mateushg17@gmail.com>
2025-11-23 19:26:39 -0300

    Inicial

--- supabase/migrations/teste.sql
+++ supabase/migrations/teste.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS public.user (
+CREATE TABLE IF NOT EXISTS public.users (
   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   email text NOT NULL UNIQUE,
   created_at timestamptz DEFAULT now()
