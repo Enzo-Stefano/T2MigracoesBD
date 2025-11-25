930b495f5df4479b25a9a1b7382ab388bfcb1fa1
unknown <mroman6@ucs.br>
2025-11-25 19:42:28 -0300

    Inicial

--- supabase/migrations/20251123234641_teste5.sql
+++
@@ -1,8 +0,0 @@
--- Criação da tabela clientes
-create table clientes (
-  id serial primary key,
-  nome text not null,
-  email text unique not null,
-  telefone text,
-  data_cadastro date default current_date
-);
