d415f3d05bbe97d62a40a0f02ca02ec3e9b4b229
mts17c <mateushg17@gmail.com>
2025-11-30 18:09:38 -0300

    Inicial

---
+++ supabase/migrations/20251123231450_teste1.sql
@@ -0,0 +1,8 @@
+-- Criação da tabela produtos
+create table produtos (
+  id uuid primary key default gen_random_uuid(),
+  nome text not null,
+  preco numeric(10,2) not null,
+  quantidade int not null,
+  criado_em timestamp with time zone default now()
+)
---
+++ supabase/migrations/20251123233922_teste2.sql
@@ -0,0 +1,5 @@
+insert into produtos (nome, preco, quantidade)
+values 
+  ('Mouse Gamer', 120.00, 10),
+  ('Teclado Mecânico', 350.00, 5),
+  ('Webcam HD', 220.90, 8)
